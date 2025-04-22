386cd86e77fc1d18e0fcf6ba68f6ae21f94aece6
Misagh <151332741+Misagh021kid@users.noreply.github.com>
2025-04-22 12:19:43 +0330

    add discord rpc

---
+++ utils/discord_rich_presence.py
@@ -0,0 +1,20 @@
+from pypresence import Presence
+import time
+
+CLIENT_ID = '1364139937892536362'
+
+def init_discord():
+    try:
+        rpc = Presence(CLIENT_ID)
+        rpc.connect()
+        rpc.update(
+            state="Version 0.0.1",
+            details="Pentesting for MC Servers",
+            start=time.time(),
+            large_image="pixeltools",
+            large_text="Minecraft Pentest Tool"
+        )
+        return rpc
+    except Exception as e:
+        print("Discord Rich Presence unavailable:", e)
+        return None
